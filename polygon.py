151b5fec6da0b0ac1831c60f006dd1c5a891d8e4
Eshani-s-nair <eshanisnair@gmail.com>
2025-10-01 13:37:25 +0530

    add polygon with predefined input

---
+++ polygon.py
@@ -0,0 +1,32 @@
+# using predifined points
+from OpenGL.GL import *
+from OpenGL.GLUT import *
+from OpenGL.GLU import *
+def init():
+    glClearColor(0.0, 0.0, 0.0, 1.0)  
+    glColor3f(1.0, 1.0, 1.0)
+    glPointSize(4.0)
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    gluOrtho2D(0, 500, 0, 500)
+def display():
+    glClear(GL_COLOR_BUFFER_BIT)  
+    glBegin(GL_POLYGON)
+    glVertex2i(100, 100)
+    glVertex2i(400, 100)
+    glVertex2i(450, 250)
+    glVertex2i(400, 400)
+    glVertex2i(100, 400)
+    glVertex2i(50, 250)
+    glEnd()
+    glFlush()    
+def main():
+    glutInit()
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB)
+    glutInitWindowSize(500, 500)
+    glutInitWindowPosition(100, 100)
+    glutCreateWindow(b"OpenGL Polygon Example")
+    init()
+    glutDisplayFunc(display)
+    glutMainLoop()    
+main()    
