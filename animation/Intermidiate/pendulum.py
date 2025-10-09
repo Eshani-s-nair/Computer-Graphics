8d82d1a732d93856ce50cba8b584f6735fe55468
Eshani-s-nair <eshanisnair@gmail.com>
2025-10-09 19:00:17 +0530

    edited pendulum code

--- animation/Intermidiate/pendulum.py
+++ animation/Intermidiate/pendulum.py
@@ -1,54 +1,69 @@
 from OpenGL.GL import *
 from OpenGL.GLU import *
 from OpenGL.GLUT import *
-from math import sin, cos, pi
-px, py = 0, 100   
-L = 100               
-theta = 30          
+from math import pi, sin, cos
+
+px, py = 0, 100      
+L = 100             
+theta = 0           
 dtheta = 1            
-max_angle = 60       
-direction = 1         # 1 for right, -1 for left
+max_angle = 60        
+direction = 1       
+
 def init():
-    glClearColor(0,0,0,1)
+    glClearColor(0, 0, 0, 1)
     glMatrixMode(GL_PROJECTION)
     glLoadIdentity()
-    gluOrtho2D(-200,200,-200,200)
+    gluOrtho2D(-200, 200, -200, 200)
+
 def draw_circle(x, y, r, num_segments=50):
     glBegin(GL_POLYGON)
     for i in range(num_segments):
-        t = 2*pi*i/num_segments
-        glVertex2f(x + r*cos(t), y + r*sin(t))
+        t = 2 * pi * i / num_segments
+        glVertex2f(x + r * cos(t), y + r * sin(t))
     glEnd()
+
 def display():
     global theta
     glClear(GL_COLOR_BUFFER_BIT)
-    rad = theta * pi/180
-    x = px + L * sin(rad)
-    y = py - L * cos(rad)
-    glColor3f(1,1,1)
+    glMatrixMode(GL_MODELVIEW)
+    glLoadIdentity()
+
+    glPushMatrix()
+    glTranslatef(px, py, 0)           
+    glRotatef(theta, 0, 0, 1)         
+
+    
+    glColor3f(1, 1, 1)
     glBegin(GL_LINES)
-    glVertex2f(px, py)
-    glVertex2f(x, y)
+    glVertex2f(0, 0)
+    glVertex2f(0, -L)                
     glEnd()
-    glColor3f(1,0,0)
-    draw_circle(x, y, 10)
-    glColor3f(0,1,0)
+
+    glColor3f(1, 0, 0)
+    draw_circle(0, -L, 10)
+
+    glPopMatrix()
+
+    glColor3f(0, 1, 0)
     draw_circle(px, py, 5)
+
     glutSwapBuffers()
+
 def update(v):
     global theta, direction
     theta += dtheta * direction
     if theta >= max_angle or theta <= -max_angle:
-        direction *= -1   
+        direction *= -1
     glutPostRedisplay()
-    glutTimerFunc(16, update, 0) 
+    glutTimerFunc(16, update, 0)
 
 def main():
-    glutInit(sys.argv)
+    glutInit()
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB)
-    glutInitWindowSize(500,500)
-    glutInitWindowPosition(100,100)
-    glutCreateWindow(b"Pendulum Animation")
+    glutInitWindowSize(500, 500)
+    glutInitWindowPosition(100, 100)
+    glutCreateWindow(b"Pendulum Animation (Rotatef)")
     init()
     glutDisplayFunc(display)
     glutTimerFunc(0, update, 0)
